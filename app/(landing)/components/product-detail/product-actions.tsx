c3029da141a261bf40263c9e784280d7264588fa
unknown <iffa.mastura03@gmail.com>
2026-01-18 20:34:03 +0700

    live-section-3

--- app/(landing)/components/product-detail/product-actions.tsx
+++ app/(landing)/components/product-detail/product-actions.tsx
@@ -4,13 +4,25 @@ import { FiArrowRight, FiChevronDown, FiChevronUp, FiShoppingBag } from "react-i
 import Button from "../ui/button";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { useCartStore } from "@/app/hooks/use-cart-store";
+import { Product } from "@/app/types";
 
-const ProductActions = () => {
+type TProductActionsProps = {
+    product: Product;
+    stock: number;
+};
+
+const ProductActions = ({product, stock}: TProductActionsProps) => {
+    const {addItem} = useCartStore();
     const {push} = useRouter();
     const [qty, setQty] = useState(1);
 
-    const goToCheckout = () => {
+    const handleAddToCart = () => {
+        addItem(product, qty)
+    }
 
+    const goToCheckout = () => {
+        push("/checkout")
     };
 
     return(
@@ -21,7 +33,7 @@ const ProductActions = () => {
                 </div>
                 <div className="flex flex-col">
                     <button className="border-b border-gray-500 cursor-pointer h-1/2 aspect-square flex items-center justify-center" 
-                    onClick={() => setQty(qty + 1)}>
+                    onClick={() => setQty(qty < stock ? qty + 1 : qty)}>
                         <FiChevronUp/></button>
                     <button className="cursor-pointer h-1/2 aspect-square flex items-center justify-center"
                     // Jika qty lebih dari satu maka dapat di kurangi, dan jika qty dibawah 1 maka tidak dapat dikurangi
@@ -29,8 +41,8 @@ const ProductActions = () => {
                         <FiChevronDown/></button>
                 </div>
             </div>
-            <Button className="px-20 w-full"><FiShoppingBag size={24}/>Add to Cart</Button>
-            <Button variant="dark" className="px-20 w-full" onClick={() => push("/checkout")}>Checkout Now<FiArrowRight size={24}/></Button>
+            <Button className="px-20 w-full" onClick={handleAddToCart}><FiShoppingBag size={24}/>Add to Cart</Button>
+            <Button variant="dark" className="px-20 w-full" onClick={goToCheckout}>Checkout Now<FiArrowRight size={24}/></Button>
         </div>
     );
 };
--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -1,19 +1,20 @@
-"use client";
-
-import { useState } from "react";
 import OrderConfirmed from "../../components/order-status/order-confirmed";
 import OrderSubmitted from "../../components/order-status/order-submitted";
+import { getTransactionById } from "@/app/services/transaction.service";
+import { TPageProps } from "../../product/[id]/page";
 
-const OrderStatus = () => {
-    const [isConfirmed, setIsConfirmed] = useState(false);
+const OrderStatus = async ({params}: TPageProps) => {
+    const {id} = await params;
+
+    const transaction = await getTransactionById(id);
+    console.log("transaction", transaction);
 
     return(
         <main className="bg-gray-100 min-h-[80vh]">
             <div className="max-w-5xl mx-auto py-20">
                 <h1 className="text-5xl font-bold text-center mb-11">Order Status</h1>
-                <div className="grid grid-cols-2 gap-14"></div>
             </div>
-            {isConfirmed ? (<OrderConfirmed/>):(<OrderSubmitted/>)}
+            {transaction.status === "paid" ? <OrderConfirmed/> : <OrderSubmitted/>}
         </main>        
     );
 };
